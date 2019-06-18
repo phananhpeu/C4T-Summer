f3d4b4de44c59a6694e4569a05c0ba2d12f4e03b
Phanh <Pap@Pa-2.local>
2019-06-18 19:02:04 +0700

    session2

--- session-1/baitap-2/2.2/baitap2.py
+++ session-1/baitap-2/2.2/baitap2.py
@@ -1,4 +1,36 @@
-print('Thu dien thoai nhe ca lop:(')
-print('hi')
-print('uodri')
-print('................................')
+# from turtle import *
+
+# for z in range(6):
+#     for i in range(360):
+#         forward(1)
+#         left(1)
+#     left(130)
+
+# mainloop()
+
+# for i in  [1 , 8 ,2 , 10]:
+#     print(i)
+# N = input("N:  ")
+# sum = 0 
+# for z in range(0, int(N)):
+#     sum = sum + z
+# print(sum)
+
+# nam_sinh = int(input("Nam Sinh:  "))
+# age = 2019 - nam_sinh 
+# if age < 12: 
+#     print("baby")
+# elif age <= 18:
+#     print("teen")
+# else: 
+#     print("adult")
+
+from random import randint
+so = randint(0,100)
+if so < 30:
+    print("Rainy")
+elif so < 60:
+    print("Cloudy")
+else:
+    print ("Sunny")
+print(so)
